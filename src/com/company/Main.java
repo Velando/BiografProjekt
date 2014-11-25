645bb482a2b3c8c2d3f1b3ddd65187bbbe940fc5
Jonas <darkone707@gmail.com>
2014-11-25 17:55:45 +0100

    danmark

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("lol");
         System.out.println("hehe");
         System.out.println("Det er sejere at skrive det her");
+        System.out.println("dinmor er en pusher");
         //asdlkjsald
         //asdkjahsdkhsajkhd
     }
