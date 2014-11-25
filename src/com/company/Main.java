3fddf026678782ea7c5c7f5cb6a2818b04139c13
Philip <philipnicolas93@hotmail.com>
2014-11-25 17:41:18 +0100

    oprydning i main (lol)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("lol");
-        System.out.println("hehe");
-        System.out.println("Det er sejere at skrive det her");
-        //asdlkjsald
-        //asdkjahsdkhsajkhd
+
     }
 }
