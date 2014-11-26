66a6dd7edbd7018c0c2fec8265267666f5f0a794
Philip <philipnicolas93@hotmail.com>
2014-11-26 14:02:17 +0100

    Ændringer i MySQL (virker stadig ikke)

--- src/com/company/MySQL.java
+++ src/com/company/MySQL.java
@@ -5,12 +5,12 @@ import java.sql.*; // Import required packages
 public class MySQL {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String database_name = "grpro";
+    static final String database_name = "KageMandBiograf";
     static final String DB_URL = "jdbc:mysql://mysql.itu.dk/" + database_name;
 
     // Database credentials
-    static final String USER = "brabrand";
-    static final String PASS = "********";
+    static final String USER = "KageMand";
+    static final String PASS = "kage100";
 
     public static void main(String[] args) {
         Connection connection = null;
@@ -20,12 +20,14 @@ public class MySQL {
             connection = DriverManager.getConnection(DB_URL, USER, PASS); // Open connection
             statement = connection.createStatement(); // Create statement
 
-            String sql = "SELECT * FROM mailing_list"; // NB: implicit semi-colon!
+            System.out.println("Connected");
+
+            String sql = "SELECT * FROM film"; // NB: implicit semi-colon!
             ResultSet rs = statement.executeQuery(sql); // *** EXECUTE QUERY! ***
             // STEP 5: Extract data from result set
             while (rs.next()) { // Retrieve data by column name
-                String email = rs.getString("email");
-                String name = rs.getString("name");
+                String email = rs.getString("film_id");
+                String name = rs.getString("navn");
                 // int id = rs.getInt("id");
                 System.out.println("Name: '" + name + "', Email: '" + email + "'"); // Display data
             }
