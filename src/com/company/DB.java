5e718d20513ccae1f51ec17e12c1857e85f3fab9
Sebastian <seb@bgaard.dk>
2014-11-26 12:07:12 +0100

    upgrade to the DB class ive implemented the functions to the db class so we know what it should be able to do.

--- src/com/company/DB.java
+++ src/com/company/DB.java
@@ -4,4 +4,30 @@ package com.company;
  * Created by Sebastian on 26-11-2014.
  */
 public class DB {
+    public DB(){
+        super();
+    }
+
+    public static String[] getReservations(){
+
+
+    }
+
+    public static String[] getFilms(){
+
+
+    }
+
+    public static void addReservations(String reservation){
+
+
+    }
+
+    public static void removeReservation(int res_id){
+
+    }
+
+    public static Boolean[][] generateSal(int forestilling){
+
+    }
 }
