da7a5d67426e021d44ad5f2821fe00d906ec67d1
Sebastian <seb@bgaard.dk>
2014-11-26 12:22:39 +0100

    Upgrade to the Ordre Class jeg har indføret hvad jeg føler er de dele der skal til for at lave ordre classen.

--- src/com/company/Ordere.java
+++ src/com/company/Ordere.java
@@ -4,4 +4,53 @@ package com.company;
  * Created by Sebastian on 26-11-2014.
  */
 public class Ordere {
+    //fields
+    // liste af alle film
+    private String[] films;
+    // liste af alle forestillinger
+    private String[] forestillinger;
+    // liste af tidspunkter til forestillinger som skal stemme overens med forstillingerne
+    private int[] tidspunkterTilForestillinger;
+    // sal til den enkelte forestilling (husk om vi skal hente dem eller ikke)
+    private Boolean[][] seats;
+
+    //the constructor
+    public Ordere(){
+        super();
+        // her skal vi hente dataen fra databasen og ligge den ind i de fields vi har i
+        // denne klasse
+    }
+
+    private void makeFilms(){
+
+    }
+
+    private void makeForestillinger(){
+
+    }
+
+    private void makeTidspukterTilForestilinger(){
+
+    }
+
+    private void makeSeats(){
+
+    }
+    // en specifik film
+    public String getFilm(int nr_film){
+
+    }
+    // en specifik forestilling
+    public String getForestiling(int nr_forstil){
+
+    }
+    // tidspunktet til en specifik forestilling
+    public int getTidspunktTilForestilling(int nr_forstil){
+
+    }
+    // salens sæder til en specifik forestilling.
+    public Boolean[][] getSeats(){
+
+    }
+
 }
