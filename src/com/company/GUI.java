d0293ebb11f55743f6eea90206c24342b9ec6ad1
Jonas <darkone707@gmail.com>
2014-12-03 14:37:52 +0100

    flere tilføjelser og ændringer til GUI

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -1,25 +1,31 @@
 package com.company;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.LineBorder;
+import javax.swing.border.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 /**
  * Created by Jonas on 03-12-2014.
  */
 public class GUI {
-    private JFrame frame;
+    private JFrame frame = new JFrame("Biograf GUI");
+    private JPanel contentPane = (JPanel) frame.getContentPane();
+    private JPanel filmPane = new JPanel(new BorderLayout(6,6));
+    private JPanel dagPane = new JPanel(new BorderLayout(6,6));
     private JPanel centerGrid = new JPanel(new GridLayout(0,3));
-    private JPanel westGrid = new JPanel(new GridLayout(0,1));
+    private JTabbedPane tabbedPane = new JTabbedPane();
+
 
     public GUI() {
         makeFrame();
     }
 
-    private void makeCenter(){
+    private void makeFilmCenter(){
+        JPanel centerGrid = new JPanel(new GridLayout(0,3));
+
         //get some ArrayList with show times for a specific movie
         ArrayList<String> l = new ArrayList<String>();
         l.add("Mandag 16:00");
@@ -33,9 +39,146 @@ public class GUI {
         for(String s: l) {
             centerGrid.add(new JButton(s));
         }
+
+        filmPane.add(centerGrid, BorderLayout.CENTER);
     }
 
-    private void makeWest(){
+    private void getMandag(){
+        //get shows for mandag
+        ArrayList<String> l = new ArrayList<String>();
+
+        l.add("gg dinfar");
+        l.add("noget andet");
+        l.add("lolololo");
+
+        centerGrid.removeAll();
+
+        for(String s: l) {
+            centerGrid.add(new JButton(s));
+        }
+
+        dagPane.add(centerGrid, BorderLayout.CENTER);
+        dagPane.revalidate();
+        dagPane.repaint();
+    }
+
+    private void getTirsdag(){
+        //get shows for tirsdag
+        ArrayList<String> l = new ArrayList<String>();
+
+        l.add("filmen her");
+        l.add("blablabla");
+        l.add("blablablabal");
+
+        centerGrid.removeAll();
+
+        for(String s: l) {
+            centerGrid.add(new JButton(s));
+        }
+
+        dagPane.add(centerGrid, BorderLayout.CENTER);
+        dagPane.revalidate();
+        dagPane.repaint();
+    }
+
+    private void getOnsdag(){
+        //get shows for onsdag
+        ArrayList<String> l = new ArrayList<String>();
+
+        l.add("filmen her");
+        l.add("blablabla");
+        l.add("blablablabal");
+
+        centerGrid.removeAll();
+
+        for(String s: l) {
+            centerGrid.add(new JButton(s));
+        }
+
+        dagPane.add(centerGrid, BorderLayout.CENTER);
+        dagPane.revalidate();
+        dagPane.repaint();
+    }
+
+    private void getTorsdag(){
+        //get shows for torsdag
+        ArrayList<String> l = new ArrayList<String>();
+
+        l.add("filmen her");
+        l.add("blablabla");
+        l.add("blablablabal");
+
+        centerGrid.removeAll();
+
+        for(String s: l) {
+            centerGrid.add(new JButton(s));
+        }
+
+        dagPane.add(centerGrid, BorderLayout.CENTER);
+        dagPane.revalidate();
+        dagPane.repaint();
+    }
+
+    private void getFredag(){
+        //get shows for fredag
+        ArrayList<String> l = new ArrayList<String>();
+
+        l.add("filmen her");
+        l.add("blablabla");
+        l.add("blablablabal");
+
+        centerGrid.removeAll();
+
+        for(String s: l) {
+            centerGrid.add(new JButton(s));
+        }
+
+        dagPane.add(centerGrid, BorderLayout.CENTER);
+        dagPane.revalidate();
+        dagPane.repaint();
+    }
+
+    private void getLørdag(){
+        //get shows for lørdag
+        ArrayList<String> l = new ArrayList<String>();
+
+        l.add("filmen her");
+        l.add("blablabla");
+        l.add("blablablabal");
+
+        centerGrid.removeAll();
+
+        for(String s: l) {
+            centerGrid.add(new JButton(s));
+        }
+
+        dagPane.add(centerGrid, BorderLayout.CENTER);
+        dagPane.revalidate();
+        dagPane.repaint();
+    }
+
+    private void getSøndag(){
+        //get shows for søndag
+        ArrayList<String> l = new ArrayList<String>();
+
+        l.add("filmen her");
+        l.add("blablabla");
+        l.add("blablablabal");
+
+        centerGrid.removeAll();
+
+        for(String s: l) {
+            centerGrid.add(new JButton(s));
+        }
+
+        dagPane.add(centerGrid, BorderLayout.CENTER);
+        dagPane.revalidate();
+        dagPane.repaint();
+    }
+
+    private void makeFilmWest(){
+        JPanel westGrid = new JPanel(new GridLayout(0,1));
+
         //get some ArrayList with movie names
         ArrayList<String> l = new ArrayList<String>();
         l.add("Langt filmnanv lolo");
@@ -47,48 +190,109 @@ public class GUI {
         for(String s: l){
             westGrid.add(new JButton(s));
         }
+
+        JPanel flow = new JPanel();
+        flow.add(westGrid);
+        flow.setBorder(new LineBorder(Color.BLACK));
+
+        filmPane.add(flow, BorderLayout.WEST);
+    }
+
+    private void makeDagWest() {
+        JPanel westGrid = new JPanel(new GridLayout(0,1));
+
+        JButton mandag = new JButton("Mandag");
+        mandag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {getMandag();}
+        });
+        JButton tirsdag = new JButton("Tirsdag");
+        tirsdag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {getTirsdag();}
+        });
+        JButton onsdag = new JButton("Onsdag");
+        onsdag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {getOnsdag();}
+        });
+        JButton torsdag = new JButton("Torsdag");
+        torsdag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {getTorsdag();}
+        });
+        JButton fredag = new JButton("Fredag");
+        fredag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {getFredag();}
+        });
+        JButton lørdag = new JButton("Lørdag");
+        lørdag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {getLørdag();}
+        });
+        JButton søndag = new JButton("Søndag");
+        søndag.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {getSøndag();}
+        });
+
+        westGrid.add(mandag);
+        westGrid.add(tirsdag);
+        westGrid.add(onsdag);
+        westGrid.add(torsdag);
+        westGrid.add(fredag);
+        westGrid.add(lørdag);
+        westGrid.add(søndag);
+
+        JPanel flow = new JPanel();
+        flow.add(westGrid);
+        flow.setBorder(new LineBorder(Color.BLACK));
+
+        dagPane.add(flow, BorderLayout.WEST);
+    }
+
+
+    private int getScale() {
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+
+        return d.width/1366;
     }
 
     private void makeFrame(){
-        frame = new JFrame("BiografGUI");
 
-        JPanel contentPane = (JPanel) frame.getContentPane();
-        contentPane.setBorder(new EmptyBorder(12, 12, 12, 12));
-
-        contentPane.setLayout(new BorderLayout(6, 6));
+        //some spacing
+        filmPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+        filmPane.setLayout(new BorderLayout(6, 6));
+        dagPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+        dagPane.setLayout(new BorderLayout(6, 6));
 
         //build WEST section of the window
-        makeWest();;
-        JPanel flow = new JPanel();
-                flow.add(westGrid);
-        flow.setBorder(new LineBorder(Color.BLACK));
-
-        contentPane.add(flow, BorderLayout.WEST);
-
-        //build NORTH section of the window
-        JPanel northGrid = new JPanel(new GridLayout(1,0));
-                northGrid.add(new JButton("Film"));
-                northGrid.add(new JButton("Dag"));
-
-        JPanel northFlow = new JPanel();
-                northFlow.add(northGrid);
-
-        contentPane.add(northFlow, BorderLayout.NORTH);
+        makeDagWest();
+        makeFilmWest();
 
         //build CENTER section of the window
-        makeCenter();
-        contentPane.add(centerGrid, BorderLayout.CENTER);
+        makeFilmCenter();
+        getMandag();
 
-        //building is done - arrange the components
+        tabbedPane.addTab("Film", filmPane);
+        tabbedPane.addTab("Dag", dagPane);
+
+        contentPane.add(tabbedPane);
+
+        //building is done - arrange the components7
+        Font font = new Font("Times New Roman", Font.PLAIN, 12*getScale());
+                frame.setFont(font);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
 
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation(d.width/2 - frame.getWidth()/2, d.height/2 - frame.getHeight()/2);
         frame.setSize(480, 320);
+        frame.setLocation(d.width/2 - frame.getWidth()/2, d.height/2 - frame.getHeight()/2);
 
         frame.setVisible(true);
-
-
-
     }
+
+    //action listeners
 }
